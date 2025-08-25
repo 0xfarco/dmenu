31c7379b4f15ac391214dca6d9758a46a5376030
0xfarco <0xfarco@gmail.com>
2025-08-25 21:45:29 +0000

    change lines and columns

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 15;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
