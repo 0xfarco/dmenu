e73aa91342c775eefca1a01495b4af840b722ee4
0xfarco <0xfarco@gmail.com>
2025-08-25 22:03:13 +0000

    change colors

--- config.h
+++ config.h
@@ -12,9 +12,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#eeeeee", "#222222" },
+	[SchemeSel] = { "#ffffff", "#a80030" },
+	[SchemeOut] = { "#eeeeee", "#222222" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 15;
